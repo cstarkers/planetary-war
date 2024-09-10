ac6e39c22ee9a09b136a37d017153a664b84eb2c
starkers <starkycampbell@gmail.com>
2024-09-10 22:34:37 +1000

    refactors hexer tests to their own file

--- utils/hexer.py
+++ utils/hexer.py
@@ -35,10 +35,7 @@ class Vertex:
                 print("Bad stuff! This matrix shape is unexpected")
                 print(x)
                 raise ValueError
-
-            
-
-
+       
     def distance(self, b):
         return sqrt((self.x-b.x)**2 +
                     (self.y-b.y)**2 +
@@ -66,8 +63,6 @@ class Vertex:
     def __repr__(self):
         return f"Vertex({self.x}, {self.y}, {self.z}), with {len(self.connected_points)} connections"
 
-
-
 def is_good_triangle(corners):
     # a good triangle is equilateral with side lenghths 2
     assert(len(corners)==3)
@@ -76,32 +71,6 @@ def is_good_triangle(corners):
             isclose(corners[1].distance(corners[2]), 2, abs_tol=0.001) and
             isclose(corners[2].distance(corners[0]), 2, abs_tol=0.001))
 
-
-
-def enumerate_triangle_centres():
-    φ = (1+sqrt(5))/2  # WILD that python lets you do this
-
-    valid_triangles=[]
-    
-    vertices=(Vertex(φ, 1,0), 
-              Vertex(φ, -1, 0),
-                Vertex(-φ, -1,0),
-                Vertex(-φ, 1,0),
-                Vertex(1, 0,φ),
-                Vertex(-1, 0,φ),
-                Vertex(-1, 0,-φ),
-                Vertex(1, 0,-φ),
-                Vertex(0, φ, 1),
-                Vertex(0, φ, -1),
-                Vertex(0, -φ, -1),
-                Vertex(0, -φ, 1))
-    
-    for possible_triangle in combinations(vertices, 3):
-        if is_good_triangle(possible_triangle):
-            valid_triangles.append(copy(possible_triangle))
-
-    return valid_triangles
-    
 
 def generate_goldberg_triangle (m, n):
     A=Vertex(0,0,0)
@@ -218,8 +187,7 @@ def build_normalised_trimap(m, n):
 
     return(new_tri, points)
 
-
-def get_normalisation_matrices(A: Vertex, B: Vertex, C: Vertex):
+def get_normalisation_matrices(A: Vertex, B: Vertex, C: Vertex) -> tuple[np.ndarray[float], np.ndarray[float]]:
     # returns a vector and a matrix such that applying the rotation described by the matrix, and then adding the translation, will map a normalised triangle onto ABC!
     # Will break horrendously if side lengths are not 2.
     # I should probably calculate and assert the side length... oh well.
@@ -278,6 +246,51 @@ def get_normalisation_matrices(A: Vertex, B: Vertex, C: Vertex):
     denormalise_trans = -1 * trans_vec
     return  (denormalise_rot, denormalise_trans)
 
+def buil_icoso_map (m:int, n:int) -> list[Vertex]:
+    φ = (1+sqrt(5))/2
+    vertices=(Vertex(φ, 1,0), 
+              Vertex(φ, -1, 0),
+                Vertex(-φ, -1,0),
+                Vertex(-φ, 1,0),
+                Vertex(1, 0,φ),
+                Vertex(-1, 0,φ),
+                Vertex(-1, 0,-φ),
+                Vertex(1, 0,-φ),
+                Vertex(0, φ, 1),
+                Vertex(0, φ, -1),
+                Vertex(0, -φ, -1),
+                Vertex(0, -φ, 1))
+    
+    triangles=[]#TODO
+
+    norm_tri, normalised_points=build_normalised_trimap(m, n)
+    
+    icoso_points=[]
+
+    for tri in triangles:
+        rot, trans =get_normalisation_matrices(tri)
+        for point in normalised_points:
+            icoso_points.append(Vertex((rot @ point.to_np_vec)+trans))
+
+def nearest_neighbours(p: Vertex, points: list[Vertex], n=1) -> list[Vertex]:
+    #find the n points closest to p
+    assert(len(points))>n
+    assert(n>0)
+    def key(b):
+        return p.distance(b)
+    points.sort(key)
+
+    #only gotta check points[0] because a point is always going to be its own nearest neighbour
+    if points[0] is p:
+        return points[1:n+1]
+    else:
+        return points[:n]            
+    
+
+def connect_icoso_points(icoso_points: list[Vertex]):
+    for point in icoso_points:
+        point.connected_points+=nearest_neighbours(point, icoso_points, 3-len(point.connected_points))
+
 
 
 
@@ -302,214 +315,3 @@ def build_icoso(m,n):
 
 
 
-
-
-# --- test plots ---
-def test_plot_tri(m,n):
-    # jsut a little test
-    points=generate_goldberg_triangle(m,n)
-    xpoints=[p.x for p in points]
-    ypoints=[p.y for p in points]
-    xpoints.append(0)
-    ypoints.append(0)
-    plt.plot(xpoints, ypoints)
-    plt.show()
-
-def test_plot_hexmap(m,n):
-    # jsut a little test
-    tri_points=generate_goldberg_triangle(m,n)
-    tri_xpoints=[p.x for p in tri_points]
-    tri_ypoints=[p.y for p in tri_points]
-    tri_xpoints.append(0)
-    tri_ypoints.append(0)
-    plt.plot(tri_xpoints, tri_ypoints)
-
-    points= generate_trimap(m, n) 
-    xpoints=[p.x for p in points]
-    ypoints=[p.y for p in points]
-    for i in range(len(xpoints)):
-        print(f"X:{xpoints[i]}, Y:{ypoints[i]}")
-
-    plt.plot(xpoints, ypoints, "x")
-    plt.gca().set_aspect('equal')
-    plt.show()
-
-
-def test_plot_rotation(m,n):
-    # jsut a little test
-    tri_points=generate_goldberg_triangle(m,n)
-    new_tri=[]
-    for p in tri_points:
-        new_tri.append(renormalise_from_triangle(p, tri_points[1]))
-
-    tri_xpoints=[p.x for p in new_tri]
-    tri_ypoints=[p.y for p in new_tri]
-    tri_xpoints.append(tri_xpoints[0])
-    tri_ypoints.append(tri_ypoints[0])
-    plt.plot(tri_xpoints, tri_ypoints)
-    plt.gca().set_aspect('equal')
-    plt.show()
-
-
-
-def test_plot_normalisation(m,n):
-    # jsut a little test
-    tri_points, points=build_normalised_trimap(m, n)
-    tri_xpoints=[p.x for p in tri_points]
-    tri_ypoints=[p.y for p in tri_points]
-    tri_xpoints.append(tri_xpoints[0])
-    tri_ypoints.append(tri_ypoints[0])
-    plt.plot(tri_xpoints, tri_ypoints)
-
-    xpoints=[p.x for p in points]
-    ypoints=[p.y for p in points]
-    for i in range(len(xpoints)):
-        print(f"X:{xpoints[i]}, Y:{ypoints[i]}")
-
-    plt.plot(xpoints, ypoints, "x")
-    plt.gca().set_aspect('equal')
-    plt.show()
-
-    plt.gca().set_aspect('equal')
-    plt.show()
-
-
-def test_plot_denormalisation():
-    # tests whether get_denornalistaion_matrices can reconstruct a rotation and trnaslation matrix
-    test_trans=np.array([[4], [3.5], [17]])
-    
-    # this equates to 37 degrees around [0.7, 0.3, 0.648...]
-    test_denorm_rot= np.array([[0.8973041, -0.3477342,  0.2718939],
-                        [0.4323072,  0.8167583, -0.3821208],
-                        [-0.0891951,  0.4604203,  0.8832086 ]])
-    test_A=Vertex((test_denorm_rot @ np.array([[0],[0],[0]])) + test_trans)
-    test_B=Vertex((test_denorm_rot @ np.array([[2],[0],[0]])) + test_trans)
-    test_C=Vertex((test_denorm_rot @ np.array([[1],[sqrt(3)],[0]])) + test_trans)
-
-    # sometimes you gotta test the test
-    assert(is_good_triangle((test_A, test_B, test_C)))
-
-    calced_denorm_rot, calced_trans = get_normalisation_matrices(test_A, test_B, test_C)
-
-    rot_good=False
-
-    if np.allclose(calced_denorm_rot, test_denorm_rot, atol=0.001):
-        print("Rot was good!")
-        rot_good=True
-    else:
-        print(f"Rot was bad :( \n The calculated rotation matrix for denormalisation: \n{calced_denorm_rot}\n")
-        print(f"The denormalisation matrix used for generating the test case: \n{test_denorm_rot}\n")
-
-    if np.allclose(calced_trans, test_trans):
-        print("Trans was good!")
-        if rot_good:
-            print("🎉🎉🎉Holy shit you did it 🎉🎉🎉🎉")
-    else:
-        print(f"Trans was bad :( \n The calculated Trans was: \n{calced_trans}")
-
-def test_plot_denormalisation_x():
-    angle = 1 # radians
-    test_denorm_rot= np.array([[1, 0,  0 ],
-                        [0, cos(angle), -sin(angle)],
-                        [0, sin(angle), cos(angle) ]])
-    
-    test_A=Vertex(test_denorm_rot @ np.array([[0],[0],[0]])) 
-    test_B=Vertex(test_denorm_rot @ np.array([[2],[0],[0]])) 
-    test_C=Vertex(test_denorm_rot @ np.array([[1],[sqrt(3)],[0]])) 
-    
-    
-    calced_denorm_rot, calced_trans = get_normalisation_matrices(test_A, test_B, test_C)
-
-    rot_good=False
-
-    if np.allclose(calced_denorm_rot, test_denorm_rot):
-        print("Rot was good!")
-        rot_good=True
-    else:
-        print(f"Rot was bad :( \n The calculated rotation matrix for denormalisation: \n{calced_denorm_rot}\n")
-        print(f"The denormalisation matrix used for generating the test case: \n{test_denorm_rot}\n")
-
-
-    
-
-def test_plot_denormalisation_y():
-    angle = 1 # radians
-    test_denorm_rot= np.array([[cos(angle), 0, sin(angle)],
-                        [0, 1, 0],
-                        [-sin(angle), 0,  cos(angle) ]])
-    
-    test_A=Vertex(test_denorm_rot @ np.array([[0],[0],[0]])) 
-    test_B=Vertex(test_denorm_rot @ np.array([[2],[0],[0]])) 
-    test_C=Vertex(test_denorm_rot @ np.array([[1],[sqrt(3)],[0]])) 
-    
-    
-
-    calced_denorm_rot, calced_trans = get_normalisation_matrices(test_A, test_B, test_C)
-
-    if np.allclose(calced_denorm_rot, test_denorm_rot):
-        print("Rot was good!")
- 
-    else:
-        print(f"Rot was bad :( \n The calculated rotation matrix for denormalisation: \n{calced_denorm_rot}\n")
-        print(f"The denormalisation matrix used for generating the test case: \n{test_denorm_rot}\n")
-
-    
-
-def test_plot_denormalisation_z():
-    angle = 1 # radians
-    test_denorm_rot= np.array([[cos(angle), -sin(angle),0 ],
-                        [ sin(angle), cos(angle), 0 ],
-                        [0, 0, 1]])
-    
-    test_A=Vertex(test_denorm_rot @ np.array([[0],[0],[0]])) 
-    test_B=Vertex(test_denorm_rot @ np.array([[2],[0],[0]])) 
-    test_C=Vertex(test_denorm_rot @ np.array([[1],[sqrt(3)],[0]])) 
-
-    
-    calced_denorm_rot, calced_trans = get_normalisation_matrices(test_A, test_B, test_C)
-
-    
-
-    if np.allclose(calced_denorm_rot, test_denorm_rot):
-        print("Rot was good!")
-        
-    else:
-        print(f"Rot was bad :( \n The calculated rotation matrix for denormalisation: \n{calced_denorm_rot}\n")
-        print(f"The denormalisation matrix used for generating the test case: \n{test_denorm_rot}\n")
-
-
-
-
-
-"""
-def position_on_icosohedron(trimap_points) -> list:
-    # positions the trimap in 3d space, mapped onto the faces of an icosohedron
-    # see https://royalsocietypublishing.org/doi/10.1098/rsos.220675 diagram 2(a)
-    return icoso_points
-
-def join_trimaps():
-    # joins the trimap corners up
-    # see https://royalsocietypublishing.org/doi/10.1098/rsos.220675 diagram 2(c)
-    return icoso_points
-
-def project_to_sphere(points):
-
-    return points
-    """
-
-
-#test_plot_normalisation(4,2)
-
-
-print("--- TESTING Y ROTATION ---")
-test_plot_denormalisation_y()
-print("\n\n")
-
-print("--- TESTING Z ROTATION ---")
-test_plot_denormalisation_z()
-print("\n\n")
-print("--- TESTING X ROTATION ---")
-test_plot_denormalisation_x()
-print("\n\n")
-print("--- TESTING COMPOUND ROTATION WITH TRANSLATION ---")
-test_plot_denormalisation()
---
+++ utils/hexer_tests.py
@@ -0,0 +1,211 @@
+from hexer import *
+
+# --- test plots ---
+def test_plot_tri(m,n):
+    # jsut a little test
+    points=generate_goldberg_triangle(m,n)
+    xpoints=[p.x for p in points]
+    ypoints=[p.y for p in points]
+    xpoints.append(0)
+    ypoints.append(0)
+    plt.plot(xpoints, ypoints)
+    plt.show()
+
+def test_plot_hexmap(m,n):
+    # jsut a little test
+    tri_points=generate_goldberg_triangle(m,n)
+    tri_xpoints=[p.x for p in tri_points]
+    tri_ypoints=[p.y for p in tri_points]
+    tri_xpoints.append(0)
+    tri_ypoints.append(0)
+    plt.plot(tri_xpoints, tri_ypoints)
+
+    points= generate_trimap(m, n) 
+    xpoints=[p.x for p in points]
+    ypoints=[p.y for p in points]
+    for i in range(len(xpoints)):
+        print(f"X:{xpoints[i]}, Y:{ypoints[i]}")
+
+    plt.plot(xpoints, ypoints, "x")
+    plt.gca().set_aspect('equal')
+    plt.show()
+
+
+def test_plot_rotation(m,n):
+    # jsut a little test
+    tri_points=generate_goldberg_triangle(m,n)
+    new_tri=[]
+    for p in tri_points:
+        new_tri.append(renormalise_from_triangle(p, tri_points[1]))
+
+    tri_xpoints=[p.x for p in new_tri]
+    tri_ypoints=[p.y for p in new_tri]
+    tri_xpoints.append(tri_xpoints[0])
+    tri_ypoints.append(tri_ypoints[0])
+    plt.plot(tri_xpoints, tri_ypoints)
+    plt.gca().set_aspect('equal')
+    plt.show()
+
+
+
+def test_plot_normalisation(m,n):
+    # jsut a little test
+    tri_points, points=build_normalised_trimap(m, n)
+    tri_xpoints=[p.x for p in tri_points]
+    tri_ypoints=[p.y for p in tri_points]
+    tri_xpoints.append(tri_xpoints[0])
+    tri_ypoints.append(tri_ypoints[0])
+    plt.plot(tri_xpoints, tri_ypoints)
+
+    xpoints=[p.x for p in points]
+    ypoints=[p.y for p in points]
+    for i in range(len(xpoints)):
+        print(f"X:{xpoints[i]}, Y:{ypoints[i]}")
+
+    plt.plot(xpoints, ypoints, "x")
+    plt.gca().set_aspect('equal')
+    plt.show()
+
+    plt.gca().set_aspect('equal')
+    plt.show()
+
+
+def test_plot_denormalisation():
+    # tests whether get_denornalistaion_matrices can reconstruct a rotation and trnaslation matrix
+    test_trans=np.array([[4], [3.5], [17]])
+    
+    # this equates to 37 degrees around [0.7, 0.3, 0.648...]
+    test_denorm_rot= np.array([[0.8973041, -0.3477342,  0.2718939],
+                        [0.4323072,  0.8167583, -0.3821208],
+                        [-0.0891951,  0.4604203,  0.8832086 ]])
+    test_A=Vertex((test_denorm_rot @ np.array([[0],[0],[0]])) + test_trans)
+    test_B=Vertex((test_denorm_rot @ np.array([[2],[0],[0]])) + test_trans)
+    test_C=Vertex((test_denorm_rot @ np.array([[1],[sqrt(3)],[0]])) + test_trans)
+
+    # sometimes you gotta test the test
+    assert(is_good_triangle((test_A, test_B, test_C)))
+
+    calced_denorm_rot, calced_trans = get_normalisation_matrices(test_A, test_B, test_C)
+
+    rot_good=False
+
+    if np.allclose(calced_denorm_rot, test_denorm_rot, atol=0.001):
+        print("Rot was good!")
+        rot_good=True
+    else:
+        print(f"Rot was bad :( \n The calculated rotation matrix for denormalisation: \n{calced_denorm_rot}\n")
+        print(f"The denormalisation matrix used for generating the test case: \n{test_denorm_rot}\n")
+
+    if np.allclose(calced_trans, test_trans):
+        print("Trans was good!")
+        if rot_good:
+            print("🎉🎉🎉Holy shit you did it 🎉🎉🎉🎉")
+    else:
+        print(f"Trans was bad :( \n The calculated Trans was: \n{calced_trans}")
+
+def test_plot_denormalisation_x():
+    angle = 1 # radians
+    test_denorm_rot= np.array([[1, 0,  0 ],
+                        [0, cos(angle), -sin(angle)],
+                        [0, sin(angle), cos(angle) ]])
+    
+    test_A=Vertex(test_denorm_rot @ np.array([[0],[0],[0]])) 
+    test_B=Vertex(test_denorm_rot @ np.array([[2],[0],[0]])) 
+    test_C=Vertex(test_denorm_rot @ np.array([[1],[sqrt(3)],[0]])) 
+    
+    
+    calced_denorm_rot, calced_trans = get_normalisation_matrices(test_A, test_B, test_C)
+
+    rot_good=False
+
+    if np.allclose(calced_denorm_rot, test_denorm_rot):
+        print("Rot was good!")
+        rot_good=True
+    else:
+        print(f"Rot was bad :( \n The calculated rotation matrix for denormalisation: \n{calced_denorm_rot}\n")
+        print(f"The denormalisation matrix used for generating the test case: \n{test_denorm_rot}\n")
+
+
+    
+
+def test_plot_denormalisation_y():
+    angle = 1 # radians
+    test_denorm_rot= np.array([[cos(angle), 0, sin(angle)],
+                        [0, 1, 0],
+                        [-sin(angle), 0,  cos(angle) ]])
+    
+    test_A=Vertex(test_denorm_rot @ np.array([[0],[0],[0]])) 
+    test_B=Vertex(test_denorm_rot @ np.array([[2],[0],[0]])) 
+    test_C=Vertex(test_denorm_rot @ np.array([[1],[sqrt(3)],[0]])) 
+    
+    
+
+    calced_denorm_rot, calced_trans = get_normalisation_matrices(test_A, test_B, test_C)
+
+    if np.allclose(calced_denorm_rot, test_denorm_rot):
+        print("Rot was good!")
+ 
+    else:
+        print(f"Rot was bad :( \n The calculated rotation matrix for denormalisation: \n{calced_denorm_rot}\n")
+        print(f"The denormalisation matrix used for generating the test case: \n{test_denorm_rot}\n")
+
+    
+
+def test_plot_denormalisation_z():
+    angle = 1 # radians
+    test_denorm_rot= np.array([[cos(angle), -sin(angle),0 ],
+                        [ sin(angle), cos(angle), 0 ],
+                        [0, 0, 1]])
+    
+    test_A=Vertex(test_denorm_rot @ np.array([[0],[0],[0]])) 
+    test_B=Vertex(test_denorm_rot @ np.array([[2],[0],[0]])) 
+    test_C=Vertex(test_denorm_rot @ np.array([[1],[sqrt(3)],[0]])) 
+
+    
+    calced_denorm_rot, calced_trans = get_normalisation_matrices(test_A, test_B, test_C)
+
+    
+
+    if np.allclose(calced_denorm_rot, test_denorm_rot):
+        print("Rot was good!")
+        
+    else:
+        print(f"Rot was bad :( \n The calculated rotation matrix for denormalisation: \n{calced_denorm_rot}\n")
+        print(f"The denormalisation matrix used for generating the test case: \n{test_denorm_rot}\n")
+
+
+
+
+
+"""
+def position_on_icosohedron(trimap_points) -> list:
+    # positions the trimap in 3d space, mapped onto the faces of an icosohedron
+    # see https://royalsocietypublishing.org/doi/10.1098/rsos.220675 diagram 2(a)
+    return icoso_points
+
+def join_trimaps():
+    # joins the trimap corners up
+    # see https://royalsocietypublishing.org/doi/10.1098/rsos.220675 diagram 2(c)
+    return icoso_points
+
+def project_to_sphere(points):
+
+    return points
+    """
+
+
+#test_plot_normalisation(4,2)
+
+
+print("--- TESTING Y ROTATION ---")
+test_plot_denormalisation_y()
+print("\n\n")
+
+print("--- TESTING Z ROTATION ---")
+test_plot_denormalisation_z()
+print("\n\n")
+print("--- TESTING X ROTATION ---")
+test_plot_denormalisation_x()
+print("\n\n")
+print("--- TESTING COMPOUND ROTATION WITH TRANSLATION ---")
+test_plot_denormalisation()
